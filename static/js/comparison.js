c4f02a7c01583c90155aee5927990c7b494a9354
Tucptak <125689687+Tucptak@users.noreply.github.com>
2024-05-10 17:00:27 +0200

    Zlepšení funkcionality js

--- static/js/comparison.js
+++ static/js/comparison.js
@@ -20,16 +20,6 @@ const get_types = {
 	}
 };
 
-const get_models = {
-	async: true,
-	crossDomain: true,
-	url: 'https://car-data.p.rapidapi.com/cars/models',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '04305f6bfcmsha61960de0a45264p1ac656jsn12283b6499d4',
-		'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
-	}
-};
 
 const get_years = {
 	async: true,
@@ -42,46 +32,90 @@ const get_years = {
 	}
 };
 
+	
+const get_models = {
+	async: true,
+	crossDomain: true,
+	url: 'https://car-data.p.rapidapi.com/cars/models',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '04305f6bfcmsha61960de0a45264p1ac656jsn12283b6499d4',
+		'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
+	}
+};
 
-$.ajax(get_years).done(function (response) {
-	response = response.sort(function(a, b){return b - a});
+
+	
+$.ajax(get_makes).done(function (response) {
 	response.forEach(element => {
-		$('#year_select_1').append(`<option value="${element}">${element}</option>`);
-		$('#year_select_2').append(`<option value="${element}">${element}</option>`);
+		$('#make_select_1').append(`<option value="${element}">${element}</option>`);
+	});
+});
+
+
+$.ajax(get_makes).done(function (response) {
+	response.forEach(element => {
+		$('#make_select_2').append(`<option value="${element}">${element}</option>`);
+	});
+});
+
+
+$("#make_select_1").change(function () {
+	
+	$.ajax(get_types).done(function (response) {
+		response.forEach(element => {
+			$('#type_select_1').append(`<option value="${element}">${element}</option>`);
+		});
+	});
+});
+
+
+$("#make_select_2").change(function () {
+	
+	$.ajax(get_types).done(function (response) {
+		response.forEach(element => {
+			$('#type_select_2').append(`<option value="${element}">${element}</option>`);
+		});
+	});
+});
+
+$("#type_select_1").change(function () {
+	
+	$.ajax(get_years).done(function (response) {
+		response = response.sort(function(a, b){return b - a});
+		response.forEach(element => {
+			$('#year_select_1').append(`<option value="${element}">${element}</option>`);
+		});
+	});
+});
+
+
+$("#type_select_2").change(function () {
+	
+	$.ajax(get_years).done(function (response) {
+		response = response.sort(function(a, b){return b - a});
+		response.forEach(element => {
+			$('#year_select_2').append(`<option value="${element}">${element}</option>`);
+		});
 	});
 });
 
 
 $("#year_select_1").change(function () {
 	
-	$.ajax(get_makes).done(function (response) {
-		response.forEach(element => {
-			$('#make_select_1').append(`<option value="${element}">${element}</option>`);
-		});
-	});
-});
-
-$("#year_select_2").change(function () {
-	
-	$.ajax(get_makes).done(function (response) {
-		response.forEach(element => {
-			$('#make_select_2').append(`<option value="${element}">${element}</option>`);
-		});
-	});
-});
-
-$("#make_select_1").change(function () {
-	
-	$.ajax(get_types).done(function (response) {
+	$.ajax(get_models).done(function (response) {
+		response = response.sort(function(a, b){return b - a});
 		response.forEach(element => {
 			$('#model_select_1').append(`<option value="${element}">${element}</option>`);
 		});
 	});
 });
 
-$("#make_select_2").change(function () {
+
+$("#year_select_2").change(function () {
 	
-	$.ajax(get_types).done(function (response) {
+	$.ajax(get_models).done(function (response) {
+		response = response.sort(function(a, b){return b - a});
 		response.forEach(element => {
 			$('#model_select_2').append(`<option value="${element}">${element}</option>`);
 		});
