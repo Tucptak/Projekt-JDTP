a3e7307c04f75d25a9f54e03468897e4887d9470
Tucptak <125689687+Tucptak@users.noreply.github.com>
2024-06-09 00:20:26 +0200

    L/D mode contacts

---
+++ static/js/contacts.js
@@ -0,0 +1,34 @@
+var body = document.body
+var box = document.querySelector('.box')
+var ball = document.querySelector('.ball')
+var navcontainer = document.querySelector('.nav-container')
+var navbar = document.querySelector('.navbar')
+var text = document.querySelectorAll('.text')
+
+
+
+check.addEventListener('change',function(){
+  if(this.checked == true){
+    box.setAttribute('style','background-color:rgb(245, 230, 230);')
+    ball.setAttribute('style','transform:translatex(100%);')
+    body.setAttribute('style','background-color:rgb(20, 20, 20); color:rgb(245, 230, 230);')
+    navcontainer.setAttribute('style','background-color:rgb(20, 20, 20); color:rgb(245, 230, 230);')
+    text.forEach(function(text) {
+		text.style.color = "white"; 
+	  });
+
+  }
+
+
+  if(this.checked == false){
+    box.setAttribute('style','background-color:rgb(20, 20, 20); rgb(245, 230, 230);')
+    ball.setAttribute('style','transform:translatex(0%);')
+    body.setAttribute('style','background-color:rgb(245, 230, 230); color:rgb(20, 20, 20);')
+    navcontainer.setAttribute('style','background-color:rgb(245, 230, 230); color:rgb(20, 20, 20;')
+    navbar.setAttribute('style','background-color:rgb(0, 0, 0);')
+    text.forEach(function(text) {
+		text.style.color = "black"; 
+	  }); 
+
+  }
+})
