27728af80286f0342c734baddae030d5fd1373fa
Tucptak <125689687+Tucptak@users.noreply.github.com>
2024-06-08 22:34:27 +0200

    L/D mode pro catalog

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,22 +1,28 @@
 var body = document.body
-var check = document.querySelector('#check')
 var box = document.querySelector('.box')
 var ball = document.querySelector('.ball')
+var navcontainer = document.querySelector('.nav-container')
+var navbar = document.querySelector('.navbar')
+
 
 
 check.addEventListener('change',function(){
   if(this.checked == true){
-    box.setAttribute('style','background-color:rgb(252, 251, 244);')
+    box.setAttribute('style','background-color:rgb(245, 230, 230);')
     ball.setAttribute('style','transform:translatex(100%);')
-    body.setAttribute('style','background-color:rgb(20, 20, 20); rgb(252, 251, 244);') 
+    body.setAttribute('style','background-color:rgb(20, 20, 20); color:rgb(245, 230, 230);')
+    navcontainer.setAttribute('style','background-color:rgb(20, 20, 20); color:rgb(245, 230, 230);')
+    navbar.setAttribute('style','background-color:rgba(0,0,0,0.7);') 
 
   }
 
 
   if(this.checked == false){
-    box.setAttribute('style','background-color:rgb(20, 20, 20); rgb(252, 251, 244);')
+    box.setAttribute('style','background-color:rgb(20, 20, 20); rgb(245, 230, 230);')
     ball.setAttribute('style','transform:translatex(0%);')
-    body.setAttribute('style','background-color:rgb(252, 251, 244); color:rgb(20, 20, 20);')
+    body.setAttribute('style','background-color:rgb(245, 230, 230); color:rgb(20, 20, 20);')
+    navcontainer.setAttribute('style','background-color:rgb(245, 230, 230); color:rgb(20, 20, 20;')
+    navbar.setAttribute('style','background-color:rgb(0, 0, 0);') 
 
   }
 })
